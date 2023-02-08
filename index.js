3e89376dc91516f53a7c1771a74a50c240f41bd0
Marjonaxon <Marjonaxon>
2023-02-08 21:36:47 +0300

    js

---
+++ index.js
@@ -0,0 +1,16 @@
+const box = document.querySelector('.box')
+const btn =document.querySelector('.btn')
+
+btn.addEventListener('click',  ()=> {
+    box.classList.toggle('play')
+})
+   
+
+
+
+    
+
+
+  
+
+
